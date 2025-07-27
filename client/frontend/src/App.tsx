cc7584dd4385ef6e365bbb6078e8658bcc76cb3b
Rakesh Reddy <bhumireddyr@valuefy.com>
2025-07-27 14:05:13 +0530

    added basic FE

---
+++ client/frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+    
+      </Routes>
+    </BrowserRouter>
+  )
+}
